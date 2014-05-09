82db4d8e0ff13b74c6775197bf1a0d6a698ee53c
Brent Schmaltz <brentschmaltz@hotmail.com>
2014-05-09 13:07:05 -0700

    code clean up.

--- TodoListWebApp/App_Start/Startup.Auth.cs
+++ TodoListWebApp/App_Start/Startup.Auth.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.Owin.Security.Cookies;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.OpenIdConnect;
-using System.Threading.Tasks;
 using System.Configuration;
-using TodoListWebApp.DAL;
 using System.IdentityModel.Claims;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TodoListWebApp.DAL;
 using TodoListWebApp.Models;
+using AuthenticationContext = Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext;
 
 namespace TodoListWebApp
 {
-
     public partial class Startup
     {
         private TodoListWebAppContext db = new TodoListWebAppContext();
+
         public void ConfigureAuth(IAppBuilder app)
         {         
             string clientId = ConfigurationManager.AppSettings["ida:ClientID"];
@@ -28,15 +29,13 @@ namespace TodoListWebApp
             string Authority = "https://login.windows.net/common/";
 
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
-
             app.UseCookieAuthentication(new CookieAuthenticationOptions { });
-
             app.UseOpenIdConnectAuthentication(
                 new OpenIdConnectAuthenticationOptions
                 {
                     Client_Id = clientId,
                     Authority = Authority,
-                    TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
+                    TokenValidationParameters = new TokenValidationParameters
                     {
                         // instead of using the default validation (validating against a single issuer value, as we do in line of business apps), 
                         // we inject our own multitenant validation logic
@@ -44,21 +43,18 @@ namespace TodoListWebApp
                     },
                     Notifications = new OpenIdConnectAuthenticationNotifications()
                     {
-                        AccessCodeReceived = (context) =>
+                       AccessCodeReceived = (context) =>
                        {
                            var code = context.Code;
 
                            ClientCredential credential = new ClientCredential(clientId, appKey);
                            string tenantID = context.ClaimsIdentity.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
                            string signedInUserID = context.ClaimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-
                            AuthenticationContext authContext = new AuthenticationContext(string.Format("https://login.windows.net/{0}", tenantID));
                            AuthenticationResult result = authContext.AcquireTokenByAuthorizationCode(
                                code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, graphResourceID);
 
-
-
-                           TokenCacheEntry tce = new TokenCacheEntry
+                           TokenCacheEntry tokenCacheEntry = new TokenCacheEntry
                            {
                                SignedInUser = signedInUserID,
                                TokenRequestorUser = result.UserInfo.UserId,
@@ -69,11 +65,14 @@ namespace TodoListWebApp
                            };
                            var existing = db.TokenCache.FirstOrDefault(a => (a.SignedInUser == signedInUserID) && (a.ResourceID == graphResourceID));
                            if (existing != null)
+                           {
                                db.TokenCache.Remove(existing);
-                           db.TokenCache.Add(tce);
+                           }
+
+                           db.TokenCache.Add(tokenCacheEntry);
                            db.SaveChanges();
                            return Task.FromResult(0);
-                       },
+                        },
                         RedirectToIdentityProvider = (context) =>
                         {
                             // This ensures that the address used for sign in and sign out is picked up dynamically from the request
@@ -84,7 +83,7 @@ namespace TodoListWebApp
                             context.ProtocolMessage.Post_Logout_Redirect_Uri = appBaseUrl;
                             return Task.FromResult(0);
                         },
-                        // we use this notification for injecting our custom logic
+                        // we use this notification for injecting our custom logic after securitytoken has been validated
                         SecurityTokenValidated = (context) =>
                         {
                             // retriever caller data from the incoming principal
@@ -98,8 +97,11 @@ namespace TodoListWebApp
                                 // the caller is recorded in the db of users who went through the individual onboardoing
                                 && (db.Users.FirstOrDefault(b => ((b.UPN == UPN) && (b.TenantID == tenantID))) == null)
                                 )
+                            {
                                 // the caller was neither from a trusted issuer or a registered user - throw to block the authentication flow
                                 throw new System.IdentityModel.Tokens.SecurityTokenValidationException();
+                            }
+
                             return Task.FromResult(0);
                         },
                         AuthenticationFailed = (context) =>
@@ -109,7 +111,6 @@ namespace TodoListWebApp
                         }
                     }
                 });
-
         }
 
     }
